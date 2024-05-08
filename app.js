8ca54ef7bf0c24792f598d41c2609a5c30cd3495
Saira Batool <sairabatool787@gmail.com>
2024-05-07 19:18:48 -0700

    Remaining Calculator Work

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // Class # 1
-alert("Hello World");
-console.log("HI");
+// alert("Hello World");
+// console.log("HI");
 
-var string = "Saira";
-var int = 65;
-var boolean = true;
-console.log(string);
+// var string = "Saira";
+// var int = 65;
+// var boolean = true;
+// console.log(string);
+
+// Practice
+// alert("Hello Saira");
+// console.log(2 + 2);
+
+// Class # 2
+var fName; //declaration
+var fname = "Sana"; //assignment
+
+var myAge = 7;
+document.write(myAge + "</br>");
+
+myAge = 6; //Re-assign
+document.write(myAge + "</br>");
+
+var fname = "Saira";
+var lname = "batool";
+
+document.write(fname + lname);  //string concatination
+
+var num = 90;
+num = num + 10;
+document.write(num);
+
+var myFristName = "Saira";  //camel Case
+var my_second_name = "Batool"; //Snake Case
