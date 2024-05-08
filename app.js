2ed0c642fd55d6c2421be21dd3742dcac004186e
Saira Batool <sairabatool787@gmail.com>
2024-05-07 19:47:18 -0700

    DONE

--- app.js
+++ app.js
@@ -24,11 +24,58 @@ document.write(myAge + "</br>");
 var fname = "Saira";
 var lname = "batool";
 
-document.write(fname + lname);  //string concatination
+document.write(fname + lname); //string concatination
 
 var num = 90;
 num = num + 10;
 document.write(num);
 
-var myFristName = "Saira";  //camel Case
+var myFristName = "Saira"; //camel Case
 var my_second_name = "Batool"; //Snake Case
+
+// Operators
+var num1 = +prompt("Please Enter First Number: ");
+var num2 = +prompt("Please Enter Second Number: ");
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var mult = num1 * num2;
+var div = num1 / num2;
+var modu = num1 % num2; //modulus Or reminder
+var pow = num1 ** num2; //power
+
+document.write("<center>"+
+  "<table border = '5px'>" +
+    "<tr>" +
+    "<th>" + "Addition:" + "</th>" +
+    "<td>" + sum + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Subtraction:" + "</th>" +
+    "<td>" + sub + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Multiplication:" + "</th>" +
+    "<td>" + mult + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Division:" + "</th>" +
+    "<td>" + div + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Modulus:" + "</th>" +
+    "<td>" + modu + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Power:" + "</th>" +
+    "<td>" + pow + "</td>" +
+    "</tr>" +
+    "</table>"+
+    "</center>"
+
+);
