c6b3c9d5540cf253f5b27fd57cea120a4bfe4666
Saira Batool <sairabatool787@gmail.com>
2024-05-28 12:39:10 +0500

    Done

---
+++ Class1-3/app.js
@@ -0,0 +1,148 @@
+
+// ****************************Class # 1*******************************
+
+alert("Hello World");
+console.log("HI");
+
+// DATA TYPE 
+var string = "Saira";
+var int = 65;
+var boolean = true;
+console.log(string);
+
+// Practice
+alert("Hello Saira");
+console.log(2 + 2);
+
+alert("Hello Coders! Here's Saira Batool and i welcome to you Guy's on coding Plateform Here I Can share much more..");
+
+console.log("Saira Batool");
+console.log("65");   // as a string
+console.log(65);     // as a number
+
+// ****************************Class # 2********************************
+
+var fName; //declaration                        var a;
+var fname = "Sana"; //assignment                a = 1;
+// Declaration + Assigment = (Initialization).  var a = 1;
+
+var myAge = 7;
+document.write(myAge + "</br>");
+
+myAge = 6; //Re-assign
+document.write(myAge + "</br>");
+
+var fname = "Saira Batool";
+var lname = "787";
+
+document.write(fname + lname + "</br>"); //string concatination //Saira Batool787
+
+var fname1 = "Saira";
+var lname1 = "Batool";
+document.write(fname1 + " " + lname1 + "</br>");
+
+var num = 90;
+num = num + 10; // OR num += 10; 
+document.write(num + "</br>"); //100
+
+var num1 = "10";
+var num2 = "20";
+
+// document.write(num1 + num2);
+// OR:
+document.write(num1 + num2 + "</br>") //1020
+
+var myFristName = "Saira";     //Camel Case
+var my_second_name = "Batool"; //Snake Case
+
+// ARITHMETIC OPERATORS
+var num1 = +prompt("Please Enter First Number: ");
+var num2 = +prompt("Please Enter Second Number: ");
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var mult = num1 * num2;
+var div = num1 / num2;
+var modu = num1 % num2; //modulus Or reminder
+var pow = num1 ** num2; //power
+
+document.write("Sum = " + sum + "<br/>"); //You can perform all the operations like this..
+
+document.write("<center>" +
+    "<table border = '5px'>" +
+    "<tr>" +
+    "<th>" + "Addition:" + "</th>" +
+    "<td>" + sum + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Subtraction:" + "</th>" +
+    "<td>" + sub + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Multiplication:" + "</th>" +
+    "<td>" + mult + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Division:" + "</th>" +
+    "<td>" + div + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Modulus:" + "</th>" +
+    "<td>" + modu + "</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<th>" + "Power:" + "</th>" +
+    "<td>" + pow + "</td>" +
+    "</tr>" +
+    "</table>" +
+    "</center>"
+);
+
+var userName = prompt("Enter Your name :");
+alert("Hello " + userName) //Hello Saira
+
+// ****************************Class # 3*******************************
+
+// INCREMENT AND DECREMENT 
+// ++num;//pre
+// num++;//post
+var a = 2;
+var b = 10;
+// a++ = a= a+1
+// a-- = a= a-1
+
+// document.write(++a + a++ + a)//3 + 3 + 4 =  10
+document.write(a++ + ++a + a + "<br/>"); // 2 + 4 + 4 = 10
+document.write(--b + b-- + b + "<br/>"); // 9 + 9 + 8 = 26
+// document.write(a++ + --a + ++b - b++) // 2 + 2 + 11 - 11
+document.write(++b + "<br/>"); //9
+document.write("Umra" + a + "</br>") // Umra2
+document.write("Umra" - a + "</br>") // NaN
+
+//PRACTICE (DRY RUN)
+// var a = 2;
+// var b = 3;
+
+// document.write(a++); //2
+// document.write(++a + --b + a++ + b-- );
+//3 + 2 + 3 + 2 = 10
+// document.write(a++ + a++ + a++ + --b + b);
+//2 + 3 + 4 + 2 + 2 = 13
+// document.write(a++ + ++a + a + a-- + b-- - b - a );
+//2 + 4 + 4 + 4 + 3 - 2 - 3 = 12
+
+// DMAS RULE    /  *  +  -
+
+var totalCost = 1 + 3 * 4;  //13
+document.write(totalCost + "<br>")
+var totalCost = 1 + (3 * 4); //13
+var totalCost = (1 + 3) * 4; //16 line by line var not required
+document.write(totalCost + "<br>")
+var resultOfComputation = (2 * 4) * 4 + 2; //34 
+resultOfComputation = (2 * 4) * (4 + 2); //48 
+document.write(resultOfComputation + "<br>")
---
+++ Class4-6/app.js
@@ -0,0 +1,171 @@
+
+// ****************************Class # 4*******************************
+var tableNum = 2;
+var counter = 1;
+var inc = 1;
+
+
+document.write(tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>" +
+    tableNum + "x" + counter++ + "=" + tableNum * inc++ + "</br>"
+);
+
+// COMPARISON OPERATORS
+document.write(3 == "3")    //true
+document.write("<br>");
+
+document.write(6 !== "6"); //true
+document.write("<br>");
+
+var isRaining = false;
+if (isRaining) {
+    document.write("Wear Raincoat ")
+}
+document.write("Umra"); //Umra
+document.write("<br>");
+
+var city = prompt("Where do you live?");
+var correctAns = "karachi";
+var score = 0;    //0  OR 1
+
+if (city === correctAns) {
+    alert("correct"); //1  OR  2
+    score++
+}
+else {
+    alert("incorrect") //0 OR 0
+}
+console.log(score)
+document.write("<br>");
+
+document.write(typeof (2 * "2")) //number
+
+// check it line by line
+var fullName = prompt("What is your name?");
+if (fullName === "saira") {
+    document.write("<h1>" + "Enter In My Home" + "</h1>");
+}
+// else{
+//     document.write("<h1>" + "Please Go Ahead" + "</h1>")
+// }
+else if (fullName === "Saira") {
+    document.write("<h1>" + "Enter In My Home" + "</h1>")
+}else {
+    document.write("<h1>" + "Please Go Ahead" + "</h1>")
+}
+
+var userName = prompt("What is your gender?", "Female");
+document.write(typeof 2 + "2"); //number2
+document.write(typeof (2 + "2")); //string 
+document.write(typeof city) //city
+
+// ****************************Class # 5*******************************
+
+var cityOfLights = prompt("Enter the name of city of lights");
+var capital = prompt("Enter capital of Pakistan")
+if (cityOfLights === "karachi") {
+    alert("Correct")
+}if (cityOfLights !== "karachi") {
+    alert("Wrong Answer")
+}
+
+var guess = +prompt("Enter number that's lucky for you");
+var choice = 6;
+var yourchoice = guess == choice;
+
+if (yourchoice) {
+    document.write("<i>" + "You win! BURHHHH" + "</i>")
+} else if (yourchoice == 4) {
+    document.write("you are close")
+} else if (yourchoice == 5) {
+    document.write("you are close")
+} else {
+    document.write("<i>" + "Try Again" + "</i>")
+}
+
+var num1 = 6;
+var num2 = 3;
+
+if (num1 < 10 && num2 > 1) {
+    document.write("True")
+}else if ((num1 == 6 && num2 == 3) || num2 == 3) {
+    document.write("Kia print hoga tm batao");
+}else if (num1 == 6 || num2 == 5) {
+    document.write("false")
+}
+
+var num = +prompt("enter a number");
+var even = num % 2 == 0;
+
+if (num > 0 && num == even) {
+    document.write("<i>" + "You Write a NEGITIVE OR EVEN" + "<i>")
+} else {
+    document.write("<i>" + "You Write a POSITIVE OR ODD" + "<i>")
+}
+
+// ****************************Class # 6*******************************
+
+var companyname = prompt("What is your previous company name?");
+var userexperience = +prompt("What is your previous experience?");
+var userskills = prompt("Do you three languages?")
+
+var companychecked = companyname === "Nisum" || "Devsinc";
+var experiencechecked = userexperience == 3;
+var skillschecked = userskills === "python" && "java";
+
+if (companychecked) {
+    if (experiencechecked) {
+        if (skillschecked) {
+            alert("Yahooo! That's GOOD");
+        }
+        else {
+            alert("Unfortunately  we are not hire you because of your sills")
+        }
+    } else {
+        alert("Your experience not matched")
+    }
+} else {
+    alert("You are not from our targeted company");
+}
+
+var mypassword = "SairaBatool";
+var userpassword = prompt("Enter Your Password Here!");
+var password = userpassword === mypassword;  //"SairaBatool" 
+
+if (userpassword === " ") {
+    alert("Please enter your password");
+} else if (password) {
+    alert("Correct!")
+}
+else {
+    alert("Incorrect password")
+}
+
+// DELETEING AND INSERTION FROM LAST 
+var classmates = ["Bushra", "Anila", "Misbah", "ayesha", "Umm-e-Abiha", "Ammarah"]
+
+document.write(classmates)
+
+document.write("</br>")
+
+classmates.push("Saira")   //strings zaroor dena hota hai 
+document.write(classmates)
+
+document.write("</br>")
+
+classmates.pop() //no parameters passed in it
+classmates.pop()
+document.write(classmates)
