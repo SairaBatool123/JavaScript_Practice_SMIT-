1ac0ac789b7b294c1c4c712db45e0a064d81e2b6
Saira Batool <sairabatool787@gmail.com>
2024-07-18 10:45:11 +0500

    In Process

---
+++ test/app.js
@@ -0,0 +1,54 @@
+// var devices = ["keyboard" , "mouse", "monitor"]
+// var out= devices.shift()
+// document.write(out)
+// var join = devices.join(" ")
+// document.write(join)
+
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= 5; j++) {
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+
+// document.write("<br> <br>")
+
+
+
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+// }
+
+
+// document.write("<br> <br>")
+
+// var rows =  +prompt("Enter tye number of rows")
+
+// for (var i = 1; i <= rows; i++) {
+//     for (var j = rows; j >= i; j--) {
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+// }
+
+// var stdName= ["Sana", "Sara","Zara"]
+// var fName = ["Ali","khan",]
+
+// for(i=0; i<stdName.length; i++){
+//     for(j=0; j<fName.length; j++){
+//         document.write(stdName[i]+" "+ fName[j] +"<br>")
+
+//     }
+// }
+
+// for(var i=1; i<=100; i=i+10){
+//     for(var j=i; j <i+10 ; j++){
+//         document.write(j+ " ")
+//     }
+//     document.write("<br>")
+// }
