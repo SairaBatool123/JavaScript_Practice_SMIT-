da92bf066a18f3c499ffc0f06fa8d609dbecaa61
Saira Batool <sairabatool787@gmail.com>
2024-07-26 09:45:39 +0500

    Done

--- Tasks/Tasks/CURRENT DATE/app.js
+++ Tasks/Tasks/CURRENT DATE/app.js
@@ -1,4 +1,13 @@
-setInterval(updateTime,1000); 
+// METHOD#1
+
+// setInterval(updateTime,1000); 
+// function updateTime(){
+//     time.innerHTML = new Date();
+// }
+
+
+// METHOD#2
 function updateTime(){
-    time.innerHTML = new Date();
+        time.innerHTML = new Date();
 }
+
