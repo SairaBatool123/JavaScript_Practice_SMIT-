bb64fd945b6b0c96a2cae5c9962f345737ef2297
Saira Batool <sairabatool787@gmail.com>
2024-08-23 06:21:49 +0500

    DONE

---
+++ 40-60/app.js
@@ -0,0 +1,15 @@
+// function sayHi() {
+//     alert("Hello world!");
+// }
+// sayHi()
+
+
+
+// function clickable(msg) {
+//     alert(msg)
+// }
+
+function zoomIn(e){
+    e.style.transform = "scale(2.5)"
+    console.log(e)
+}
