44e522f5fce3c3b68bb08e70ee1684ab00e6a1b4
Saira Batool <sairabatool787@gmail.com>
2024-08-27 06:58:31 +0500

    Done

--- 40-60/app.js
+++ 40-60/app.js
@@ -9,7 +9,14 @@
 //     alert(msg)
 // }
 
-function zoomIn(e){
-    e.style.transform = "scale(2.5)"
-    console.log(e)
-}
+// function zoomIn(e){
+//     e.style.transform = "scale(2.5)"
+//     console.log(e)
+// }
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes[0]);
+// console.log(document.childNodes[1].childNodes[0].childNodes[5].childNodes[0]);
+// console.log(document.childNodes[1].childNodes[0].childNodes[5].childNodes[0]);
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[1]);
+var Val = document.getElementsByTagName('p')[2]
+console.log(Val)
