f2b9e453a7b463462a130eeb9a55a52a8ef2703b
Saira Batool <sairabatool787@gmail.com>
2024-09-04 20:15:10 +0500

    Done

---
+++ object/app.js
@@ -0,0 +1,9 @@
+var object = {
+    name: "saira",
+    fname : "Gh.Hussain",
+    Class : "BSCS"
+}
+console.log(object);
+console.log(object.name[0]); //s
+// console.log()
+console.log(object.name);
